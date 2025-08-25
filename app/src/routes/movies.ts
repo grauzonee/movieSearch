496da098453e1964bab9f0da96d96ab3b0f0c314
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-08-25 11:03:19 +0200

    Implement request body validation for post uploading request + fix volume for elastic container

--- app/src/routes/movies.ts
+++ app/src/routes/movies.ts
@@ -2,12 +2,20 @@ import { Router, Request, Response } from 'express';
 import multer from "multer"
 import { addMovieJob } from '@queue/movieQueue';
 import { addFileJob } from '@queue/fileQueue';
+import { postSchema } from '@schemas/postSchema';
+import { ValidationErrorItem } from 'joi';
 
 export const router = Router();
 const upload = multer({ dest: "uploads/" });
 
 router.post(
     '/movies', async (req: Request, res: Response) => {
+        const { error } = postSchema.validate(req.body);
+        if (error) {
+            const message = error.details.map((i: ValidationErrorItem) => { return i.message }).join(',');
+            res.status(400).json({ success: false, message })
+            return;
+        }
         const { title, plot, genres } = req.body;
         await addMovieJob({ title, plot, genres });
         res.status(200).json({ success: true })
