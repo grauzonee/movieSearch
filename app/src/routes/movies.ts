f02b0b41c3627cc7a152c90ad23da4ef4cb37cd9
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-08-26 15:24:22 +0200

    Add cors + input data sanitizing

--- app/src/routes/movies.ts
+++ app/src/routes/movies.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from 'express';
 import multer from "multer"
 import { addMovieJob } from '@queue/movieQueue';
 import { addFileJob } from '@queue/fileQueue';
-import { postSchema } from '@schemas/postSchema';
+import { movieSchema } from '@schemas/movieSchema';
 import { ValidationErrorItem } from 'joi';
 
 export const router = Router();
@@ -10,13 +10,14 @@ const upload = multer({ dest: "uploads/" });
 
 router.post(
     '/movies', async (req: Request, res: Response) => {
-        const { error } = postSchema.validate(req.body);
+        const { value, error } = movieSchema.validate(req.body);
         if (error) {
             const message = error.details.map((i: ValidationErrorItem) => { return i.message }).join(',');
             res.status(400).json({ success: false, message })
             return;
         }
-        const { title, plot, genres } = req.body;
+        const { title, plot, genres } = value;
+        console.log("title", title, plot, genres)
         await addMovieJob({ title, plot, genres }, 1, 1);
         res.status(200).json({ success: true })
     }
